a507790cf6928a278a2462cb271be83af9a4ad0e
ediblesushi <chrisdoucette15@gmail.com>
2018-10-30 12:20:14 -0400

    Fixed error handling of HTTP Errors

--- ediblepwn/ediblepwn.py
+++ ediblepwn/ediblepwn.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 BASE_URL = "https://haveibeenpwned.com/api/v2"
 HEADERS = {
@@ -10,31 +11,61 @@ class EdiblePwn():
         pass
 
     def get_breaches(self):
-        response = requests.get("{}/breaches".format(BASE_URL), headers=HEADERS)
-        if response.status_code == 200:
-            return response.json()
-        response.raise_for_status()
+        try:
+            response = requests.get("{}/breaches".format(BASE_URL), headers=HEADERS)
+            if response.text:
+                return response.json()
+            else:
+                return "No results found."
+        except requests.exceptions.HTTPError as e:
+            return e
+        except ConnectionError as e:
+            return e
 
     def get_breaches_by_account(self, account):
-        response = requests.get("{}/breachedaccount/{}".format(BASE_URL,account),headers=HEADERS)
-        if response.status_code == 200:
-            return response.json()
-        response.raise_for_status()
+        try:
+            response = requests.get("{}/breachedaccount/{}".format(BASE_URL,account),headers=HEADERS)
+            if response.text:
+                return response.json()
+            else:
+                return "No results found for specified account."
+        except requests.exceptions.HTTPError as e:
+            return e
+        except ConnectionError as e:
+            return e
 
     def get_breaches_by_site(self, site):
-        response = requests.get("{}/breach/{}".format(BASE_URL, site), headers=HEADERS)
-        if response.status_code == 200:
-            return response.json()
-        response.raise_for_status()
+        try:
+            response = requests.get("{}/breach/{}".format(BASE_URL, site), headers=HEADERS)
+            if response.text:
+                return response.json()
+            else:
+                return "No results found for specified site."
+        except requests.exceptions.HTTPError as e:
+            return e
+        except ConnectionError as e:
+            return e
 
     def get_pastes_by_account(self, account):
-        response = requests.get("{}/pasteaccount/{}".format(BASE_URL,account),headers=HEADERS)
-        if response.status_code == 200:
-            return response.json()
-        response.raise_for_status()
+        try:
+            response = requests.get("{}/pasteaccount/{}".format(BASE_URL,account),headers=HEADERS)
+            if response.text:
+                return response.json()
+            else:
+                return "No results found for specified account."
+        except requests.exceptions.HTTPError as e:
+            return e
+        except requests.exceptions.ConnectionError as e:
+            return e
 
     def get_passwords_by_partial_hash(self, hash):
-        response = requests.get("https://api.pwnedpasswords.com/range/{}".format(hash),headers=HEADERS)
-        if response.status_code == 200:
-            return response.text
-        response.raise_for_status()
+        try:
+            response = requests.get("https://api.pwnedpasswords.com/range/{}".format(hash),headers=HEADERS)
+            if response.text:
+                return response.text
+            else:
+                return "No results found for specified partial hash."
+        except requests.exceptions.HTTPError as e:
+            return e
+        except ConnectionError as e:
+            return e
